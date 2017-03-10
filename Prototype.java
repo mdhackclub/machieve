f6323207e78291fd3152ca3148d79bc320b6d3ba
Mingjie Jiang <jiang@mingjie.info>
2017-03-10 08:40:04 -0500

    Update Prototype.java

--- Prototype.java
+++ Prototype.java
@@ -2,13 +2,21 @@
  * A game by Mingjie Jiang. Idea created with Joey D. Kim. 
  * Source code available at https://github.com/mdhackclub/machieve
  * A work licensed under GNU GPL v3. Worked inside Hack Club.
+ *
+ *     ____               _           __     __  ___           __    _              
+ *    / __ \_________    (_)__  _____/ /_   /  |/  /___ ______/ /_  (_)__ _   _____ 
+ *   / /_/ / ___/ __ \  / / _ \/ ___/ __/  / /|_/ / __ `/ ___/ __ \/ / _ \ | / / _ \
+ *  / ____/ /  / /_/ / / /  __/ /__/ /_   / /  / / /_/ / /__/ / / / /  __/ |/ /  __/
+ * /_/   /_/   \____/_/ /\___/\___/\__/  /_/  /_/\__,_/\___/_/ /_/_/\___/|___/\___/ 
+ *                 /___/                                                            
  */
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 public class Prototype {
-	
 	//Global Variable Initialization
 	public static int 
 	init = 320, //Initial Value
-	power = 320, //Ruler’s Power (int, 0-1000)
+	power = 320, //Ruler's Power (int, 0-1000)
 	economy = 320, //Economy (int, 0-1000)
 	popularity = 320, //Poll Popularity/Support Rate (int, 0-1000)
 	religion = 320, //Religion (int, 0-1000)
@@ -17,8 +25,23 @@ public class Prototype {
 	foreign = 320; //Foreign Relations (int, 0-1000)
 	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		println("Hi... Welcome!");
+		
+		println("The old king has died. You are in rule of the country.");
 
-
+		println("What is your name?");
+		
+		println("[Hint: The program is waiting for your input at the \">\" symbol.]");
+		
+		print("It is King > ");
+		String userName = sc.nextLine();
+		
+		print("Welcome, King " + userName +"! The Kingdom is awaiting.");
+		sc.nextLine();
+		
+		flush();
 	}
 
 	public static void changeFac(int varId, int value) {
@@ -34,6 +57,30 @@ public class Prototype {
 		Prototype.military = Prototype.init;
 		Prototype.balance = Prototype.init;
 		Prototype.foreign = Prototype.init;
+	}
+	
+	public static void println(String str) {
+		for (int i=0; i<str.length(); i++) {
+			System.out.print(str.charAt(i));
+			try{
+				Thread.sleep(20);
+			} catch (Exception e) {}
+		}
+		System.out.println();
+	}
+	
+	public static void print(String str) {
+		for (int i=0; i<str.length(); i++) {
+			System.out.print(str.charAt(i));
+			try{
+				Thread.sleep(20);
+			} catch (Exception e) {}
+		}
+	}
+	
+	public static void flush() {
+		for (int i=0; i<200; i++)
+			System.out.println();
 	}
 
 }
