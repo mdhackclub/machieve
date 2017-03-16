6aae6b058420de647323b6c98af4265f32f51757
casprussian <anonymous.tobias@gmail.com>
2017-03-16 16:19:40 -0400

    Add string for player name to Game class

--- Game.hpp
+++ Game.hpp
@@ -1,8 +1,11 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
+#include <string>
+
 class Game {
 	int power, economy, popularity, religion, military, balance, foreign;
+	std::string name;
 public:
 	void Game();
 }
