ded0936b97ce1775dc66f67f161ba518c9ef47bf
casprussian <anonymous.tobias@gmail.com>
2017-03-17 09:17:23 -0400

    Create Game.cpp

---
+++ Game.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "Game.hpp"
+#include "input.hpp"
+
+void Game::Game() {
+
+}
+
+void Game::load() {
+
+}
+
+void Game::welcome() {
+	std::cout << "Hello and welcome!" << std::endl;
+	std::cout << "The old king is dead. You are now in control of the country." << std::endl;
+	name = input("What is your name?");
+	std::cout << "Welcome, " << name << "! The Kingdom is waiting!" << std::endl;
+}
+
+bool Game::still_alive() {
+
+}
+
+void Game::step() {
+	year++;
+	std::cout << "Year: " << year << "A.D." << std::endl;
+	//
+}
