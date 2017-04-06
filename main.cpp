4c4d16e0d9f44d21d17c275aa541d58127e24f06
casprussian <joey.d.kim@outlook.com>
2017-04-06 08:40:41 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,17 @@
 
 #include "init.hpp"
 #include "change_fac.hpp"
+#include "introduction.hpp"
+#include "input.hpp"
 
 int main(int argc, char** argv) {
+	std::cout << "A game by Joey Kim and Mingjie Jiang." << std::endl;
+	std::cout << "Licenced under GNU GPL v3, sponsored by Hack Club." << std::endl;
+	std::cout << "Welcome to the Machieve world." << std::endl;
+	std::cout << "\033[2J\033[1;1H" << std::endl;
+	std::cout << "Hello and welcome!" << std::endl;
+	std::cout << "The old king is dead. You are now in control of the nation." << std::endl;
+	// name = inp
 	const int
 	init       = 320,
 	power      = 320,
